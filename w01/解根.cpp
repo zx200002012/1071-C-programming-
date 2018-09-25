84e064fee8405b479b134cfc119315fa20e92bc5
zx200002012 <43335052+zx200002012@users.noreply.github.com>
2018-09-26 00:11:51 +0800

    Update 解根.cpp

--- w01/解根.cpp
+++ w01/解根.cpp
@@ -1,17 +1,33 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-	int A,B,C; double X1,X2;
-	printf("øÈ§JA B C:");
+	int A,B,C,i,Z,D,W; double X1,X2;
+	W=3;
+	while(W>=0){
+	printf("Ëº∏ÂÖ•A B C:");
 	scanf("%d %d %d",&A,&B,&C);
-	printf("∏— AX^2+BX+C=0 ®‚Æ⁄¨∞:\n");
-	if(B*B-4*A*C>=0){
+	printf("Ëß£ AX^2+BX+C=0 ÂÖ©Ê†πÁÇ∫:\n");
+	Z=B*B-4*A*C;
+	D=2*A;
+	
+
+	for (i = 1; i <= 1000;i=i+1);
+	if(B*B-4*A*C == (i*i) || B*B-4*A*C!=0 && B*B-4*A*C>0){
 		X1=(-B+sqrt(B*B-4*A*C))/(2*A);
 		X2=(-B-sqrt(B*B-4*A*C))/(2*A);
 		printf("X1=%.1f\n",X1);
-		printf("X2=%.1f",X2);
+		printf("X2=%.1f\n",X2);
+	}	
+	else if(B*B-4*A*C==0){
+		X1=(-B+sqrt(B*B-4*A*C))/(2*A);
+		printf("ÈáçÊ†π X=%.1f\n",X1);
 	}
-	else{
-		printf("µL∏—");
+	else if(B*B-4*A*C<0){
+		printf("ÁÑ°Ëß£\n");
 	}
+	else {
+		printf("X1=(-%d+Ê†πËôü%d)/%d\n",B,Z,D);
+		printf("X2=(-%d-Ê†πËôü%d)/%d\n",B,Z,D);
+	}
+    }
 }
